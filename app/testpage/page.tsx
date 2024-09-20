6b0fd070268e65fd7a9655a2bbb7b93e8bc88414
kazu <test@gmail.com>
2024-09-20 09:39:10 +0900

    改修作業

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
+
+// Postの型定義
+interface Post {
+  title: string;
+  body: string;
+}
+
 export default function Home() {
+  const [post, setPost] = useState<Post | null>(null); // Postかnull
+  const [loading, setLoading] = useState<boolean>(true); // ローディング状態
+  const [error, setError] = useState<Error | null>(null); // エラーかnull
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPost(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
-    <h1>
-      <p>HELLO</p>
-    </h1>
+    <div>
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
+    </div>
   );
 }
